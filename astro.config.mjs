1b6681ad8e578fbc8e6de24fa28428c39cf5476b
ariel216 <arielespinoza216@gmail.com>
2024-03-14 22:54:26 -0400

    add: styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
-
+import preact from '@astrojs/preact';
 import tailwind from "@astrojs/tailwind";
 import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), preact()],
   output:'server',
   adapter: node({
     mode: 'standalone',
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,45 @@
+export default function Formulario() {
+    //console.log('carga')
+    return (
+        <>
+            <form class="bg-gray-200 shadow-md rounded px-6 pt-4 pb-6 mb-4">
+                <div class="mb-3">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                        Username
+                    </label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight border-indigo-900 focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+                </div>
+                <div class="mb-3">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                        Username
+                    </label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight border-indigo-900 focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+                </div>
+                <div class="mb-3">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                        Username
+                    </label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight border-indigo-900 focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+                </div>
+                <div class="mb-3">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                        Username
+                    </label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight border-indigo-900 focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+                </div>
+                <div class="mb-3">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                        Username
+                    </label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight border-indigo-900 focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+                </div>
+                <div class="mb-3">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                        Username
+                    </label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight border-indigo-900 focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+                </div>
+            </form>
+        </>
+    )
+}
